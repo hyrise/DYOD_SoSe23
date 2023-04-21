0b75250a692cc16fb72095e5b8f1a0cf53c581cc
Sandro Speh <sandrospeh@gmail.com>
2023-04-21 21:41:37 +0200

    added hello world print to playground

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -7,6 +7,7 @@ using namespace opossum;  // NOLINT(build/namespaces)
 
 int main() {
   Assert(true, "We can use opossum files here :)");
+  std::cout << "Hello World" << std::endl;
 
   return 0;
 }
