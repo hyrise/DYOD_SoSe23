2d5d2d12d76a1ecd0dd1995a1c6cfdebc0c9c1d2
Daniel Lindner <27929897+dey4ss@users.noreply.github.com>
2023-05-08 21:33:37 +0200

    Sprint 2 (#3)

---
+++ src/test/storage/dictionary_segment_test.cpp
@@ -0,0 +1,73 @@
+#include "base_test.hpp"
+
+#include "resolve_type.hpp"
+#include "storage/abstract_attribute_vector.hpp"
+#include "storage/abstract_segment.hpp"
+#include "storage/dictionary_segment.hpp"
+
+namespace opossum {
+
+class StorageDictionarySegmentTest : public BaseTest {
+ protected:
+  std::shared_ptr<ValueSegment<int32_t>> value_segment_int{std::make_shared<ValueSegment<int32_t>>()};
+  std::shared_ptr<ValueSegment<std::string>> value_segment_str{std::make_shared<ValueSegment<std::string>>(true)};
+};
+
+TEST_F(StorageDictionarySegmentTest, CompressSegmentString) {
+  value_segment_str->append("Bill");
+  value_segment_str->append("Steve");
+  value_segment_str->append("Alexander");
+  value_segment_str->append("Steve");
+  value_segment_str->append("Hasso");
+  value_segment_str->append("Bill");
+  value_segment_str->append(NULL_VALUE);
+
+  const auto dict_segment = std::make_shared<DictionarySegment<std::string>>(value_segment_str);
+
+  // Test attribute_vector size.
+  EXPECT_EQ(dict_segment->size(), 7);
+
+  // Test dictionary size (uniqueness).
+  EXPECT_EQ(dict_segment->unique_values_count(), 4);
+
+  // Test sorting.
+  const auto& dict = dict_segment->dictionary();
+  EXPECT_EQ(dict[0], "Alexander");
+  EXPECT_EQ(dict[1], "Bill");
+  EXPECT_EQ(dict[2], "Hasso");
+  EXPECT_EQ(dict[3], "Steve");
+
+  // Test NULL value handling.
+  EXPECT_EQ(dict_segment->attribute_vector()->get(6), dict_segment->null_value_id());
+  EXPECT_EQ(dict_segment->get_typed_value(6), std::nullopt);
+  EXPECT_THROW(dict_segment->get(6), std::logic_error);
+}
+
+TEST_F(StorageDictionarySegmentTest, LowerUpperBound) {
+  for (auto value = int16_t{0}; value <= 10; value += 2) {
+    value_segment_int->append(value);
+  }
+
+  std::shared_ptr<AbstractSegment> segment;
+  resolve_data_type("int", [&](auto type) {
+    using Type = typename decltype(type)::type;
+    segment = std::make_shared<DictionarySegment<Type>>(value_segment_int);
+  });
+  auto dict_segment = std::dynamic_pointer_cast<DictionarySegment<int32_t>>(segment);
+
+  EXPECT_EQ(dict_segment->lower_bound(4), ValueID{2});
+  EXPECT_EQ(dict_segment->upper_bound(4), ValueID{3});
+
+  EXPECT_EQ(dict_segment->lower_bound(AllTypeVariant{4}), ValueID{2});
+  EXPECT_EQ(dict_segment->upper_bound(AllTypeVariant{4}), ValueID{3});
+
+  EXPECT_EQ(dict_segment->lower_bound(5), ValueID{3});
+  EXPECT_EQ(dict_segment->upper_bound(5), ValueID{3});
+
+  EXPECT_EQ(dict_segment->lower_bound(15), INVALID_VALUE_ID);
+  EXPECT_EQ(dict_segment->upper_bound(15), INVALID_VALUE_ID);
+}
+
+// TODO(student): You should add some more tests here (full coverage would be appreciated) and possibly in other files.
+
+}  // namespace opossum
