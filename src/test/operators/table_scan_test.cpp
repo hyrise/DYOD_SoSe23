8981273cfd8fe0bd03f716b4a3f44abd28f9293a
dey4ss <27929897+dey4ss@users.noreply.github.com>
2023-05-30 23:19:57 +0200

    update data type in test case

--- src/test/operators/table_scan_test.cpp
+++ src/test/operators/table_scan_test.cpp
@@ -97,7 +97,7 @@ TEST_F(OperatorsTableScanTest, DoubleScan) {
   auto scan_1 = std::make_shared<TableScan>(_table_wrapper, ColumnID{0}, ScanType::OpGreaterThanEquals, 1234);
   scan_1->execute();
 
-  auto scan_2 = std::make_shared<TableScan>(scan_1, ColumnID{1}, ScanType::OpLessThan, 457.9);
+  auto scan_2 = std::make_shared<TableScan>(scan_1, ColumnID{1}, ScanType::OpLessThan, 457.9f);
   scan_2->execute();
 
   EXPECT_TABLE_EQ(scan_2->get_output(), expected_result);
